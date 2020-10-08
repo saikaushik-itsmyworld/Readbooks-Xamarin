45606c4c38b0a84f807c48b096aaaa140213dd1d
Kaushik Mudiki <kmudiki@stealthpath.com>
2020-10-08 11:41:54 -0400

    Initial Commit

---
+++ Readbooks/Readbooks/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Readbooks.ViewModels;
+
+namespace Readbooks.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Readbooks/Readbooks/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Readbooks.Models;
+using Readbooks.ViewModels;
+
+namespace Readbooks.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
