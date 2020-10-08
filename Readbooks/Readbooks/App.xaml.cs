8aa4e1dd53930a9dd18b18ad4ddf8e84efaee40e
Kaushik Mudiki <kmudiki@stealthpath.com>
2020-10-08 11:47:41 -0400

    Updated App Start

--- Readbooks/Readbooks/App.xaml.cs
+++ Readbooks/Readbooks/App.xaml.cs
@@ -3,6 +3,9 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Readbooks.Services;
 using Readbooks.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace Readbooks
 {
@@ -19,6 +22,8 @@ namespace Readbooks
 
         protected override void OnStart()
         {
+            AppCenter.Start("ios=5404f2e7-a091-414b-bfa8-8c3f0195feac;android=05697554-02d1-4f07-b442-e55fcec1fd77", typeof(Analytics), typeof(Crashes));
+
         }
 
         protected override void OnSleep()
